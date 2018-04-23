7ee91af8fe44ad9d14efb2597e60f3f8f8b130ea
C <C@Cs-MacBook-Pro.local>
2018-04-23 18:14:16 -0500

    Initial Commit

---
+++ hello.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type JSONMessage struct {
+	Message string
+}
+
+func main() {
+
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/message", JSONIndex)
+	router.HandleFunc("/{urlMessage}", MessageIndex)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World")
+}
+
+func MessageIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", mux.Vars(r)["urlMessage"])
+}
+
+func JSONIndex(w http.ResponseWriter, r *http.Request) {
+	messageJSON := JSONMessage{Message: "Hello"}
+
+	json.NewEncoder(w).Encode(messageJSON)
+}
